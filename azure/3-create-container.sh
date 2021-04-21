353da7a83cb2e1cb7672b3bbb8745626559280a5
Peter Marnef <peter.marnef@plantyn.com>
2021-04-21 22:40:55 +0200

    Link file share to container instance

--- azure/3-create-container.sh
+++ azure/3-create-container.sh
@@ -2,21 +2,34 @@
 
 source config.sh
 
-echo "Creating Azure Container Instance $CONTAINER"
-if [[ $CONTAINER_REGISTRY_PASSWORD -eq "" ]]
-then
-    echo "ERROR: Container Registry password not found. Copy/paste the admin password of the container registry '$CONTAINER_REGISTRY' to the CONTAINER_REGISTRY_PASSWORD environment variable in the config.sh file before executing this file. You can find the password in the section 'Access keys'. (If the password contains an escapable char, regenerate the pwd.)"
-else
-    az container create \
-        -g $RESOURCE_GROUP \
-        --name $DOCKER_CONTAINER \
-        --image $DOCKER_CONTAINER:$DOCKER_CONTAINER_TAG \
-        --ports 2456 2457 \
-        --dns-name-label $DOCKER_CONTAINER \
-        --image $CONTAINER_REGISTRY.azurecr.io/$DOCKER_CONTAINER:$DOCKER_CONTAINER_TAG \
-        --registry-username $CONTAINER_REGISTRY \
-        --registry-password $CONTAINER_REGISTRY_PASSWORD \
-        --cpu 4 \
-        --memory 2 \
-        --protocol UDP
-fi
+CONTAINER_REGISTRY_KEY=$(
+    az acr credential show \
+    -n $CONTAINER_REGISTRY \
+    --query "passwords[0].value" \
+    --output tsv \
+)
+
+STORAGE_ACCOUNT_KEY=$(
+    az storage account keys list \
+    --resource-group $RESOURCE_GROUP \
+    --account-name $STORAGE_ACCOUNT \
+    --query "[0].value" \
+    --output tsv \
+)
+
+az container create \
+    -g $RESOURCE_GROUP \
+    --name $DOCKER_CONTAINER \
+    --image $DOCKER_CONTAINER:$DOCKER_CONTAINER_TAG \
+    --ports 2456 2457 \
+    --dns-name-label $DOCKER_CONTAINER \
+    --image $CONTAINER_REGISTRY.azurecr.io/$DOCKER_CONTAINER:$DOCKER_CONTAINER_TAG \
+    --registry-username $CONTAINER_REGISTRY \
+    --registry-password $CONTAINER_REGISTRY_KEY \
+    --cpu 4 \
+    --memory 2 \
+    --protocol UDP \
+    --azure-file-volume-account-name $STORAGE_ACCOUNT \
+    --azure-file-volume-account-key $STORAGE_ACCOUNT_KEY \
+    --azure-file-volume-share-name $STORAGE_ACCOUNT_FILESHARE \
+    --azure-file-volume-mount-path $DOCKER_CONTAINER_FILESHARE_PATH
