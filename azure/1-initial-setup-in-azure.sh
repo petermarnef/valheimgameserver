9622e71dc8d8737c5ea61b2b07674f9f54668056
Peter Marnef <peter.marnef@plantyn.com>
2021-04-21 21:48:07 +0200

    Storage account and file share creation in Azure

--- azure/1-initial-setup-in-azure.sh
+++ azure/1-initial-setup-in-azure.sh
@@ -5,7 +5,7 @@ source config.sh
 echo "Creating Resource Group $RESOURCE_GROUP"
 az group create \
     --name $RESOURCE_GROUP \
-    --location $RESOURCE_GROUP_LOCATION
+    --location $LOCATION
 
 echo "Creating Container Registry $CONTAINER_REGISTRY"
 az acr create \
@@ -13,3 +13,16 @@ az acr create \
     --name $CONTAINER_REGISTRY \
     --sku Standard \
     --admin-enabled
+
+
+echo "Creating Storage Account $STORAGE_ACCOUNT"
+az storage account create \
+    --resource-group $RESOURCE_GROUP \
+    --name $STORAGE_ACCOUNT \
+    --location $LOCATION \
+    --sku Standard_LRS
+
+echo "Create File Share $STORAGE_ACCOUNT_FILESHARE on Storage Account $STORAGE_ACCOUNT"
+az storage share create \
+  --name $STORAGE_ACCOUNT_FILESHARE \
+  --account-name $STORAGE_ACCOUNT
--- azure/config.sh
+++ azure/config.sh
@@ -2,9 +2,14 @@
 
 # Set shared environment variables
 
+LOCATION="westeurope"
+
 RESOURCE_GROUP="valheimgameserver"
-RESOURCE_GROUP_LOCATION="westeurope"
+
 CONTAINER_REGISTRY=""
 CONTAINER_REGISTRY_PASSWORD=""
 DOCKER_CONTAINER="valheimgameserver"
 DOCKER_CONTAINER_TAG="0.1"
+
+STORAGE_ACCOUNT="valheimgameserver"
+STORAGE_ACCOUNT_FILESHARE="valheimgameserverbackup"
