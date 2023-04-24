17153a9cd1fb6aa655a64df3b2201a1f414a7250
jagohr <jagohr@microsoft.com>
2023-04-24 11:13:15 -0700

    undoing changes to run-local

--- docker/run-local-interactive.sh
+++ docker/run-local-interactive.sh
@@ -2,8 +2,4 @@
 
 source config.sh
 
-docker run --name localvalheim -d -i -t $DOCKER_CONTAINER:$DOCKER_CONTAINER_TAG /bin/sh
-
-docker exec -it localvalheim /bin/bash
-
-#docker run -it -p 2456-2458:2456-2458/tcp -p 2456-2458:2456-2458/udp --rm $DOCKER_CONTAINER:$DOCKER_CONTAINER_TAG
+docker run -it -p 2456-2458:2456-2458/tcp -p 2456-2458:2456-2458/udp --rm $DOCKER_CONTAINER:$DOCKER_CONTAINER_TAG
